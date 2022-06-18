d3195dff5ae69790f5ad9a761a2426d84b819133
Geoff Bourne <itzgeoff@gmail.com>
2022-06-18 13:31:11 -0500

    Support build-time image labels

--- src/main/java/me/itzg/simpleimg/BootImageExtension.java
+++ src/main/java/me/itzg/simpleimg/BootImageExtension.java
@@ -1,10 +1,12 @@
 package me.itzg.simpleimg;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 import javax.inject.Inject;
 import org.gradle.api.Project;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
 
 public abstract class BootImageExtension {
 
@@ -30,6 +32,18 @@ public abstract class BootImageExtension {
 
     abstract ListProperty<String> getPlatforms();
 
+    abstract Property<String> getImageDescription();
+
+    abstract Property<String> getImageTitle();
+
+    abstract Property<String> getImageVersion();
+
+    abstract Property<String> getImageRevision();
+
+    abstract Property<String> getImageSourceUrl();
+
+    abstract ListProperty<String> getExtraImageLabels();
+
     /**
      * Indicates if the built image should use Spring Boot's layertools and index or just
      * bundle and execute the jar as-is.
@@ -40,12 +54,30 @@ public abstract class BootImageExtension {
     public BootImageExtension(Project project) {
         getBaseImage().convention("eclipse-temurin:17");
         getExposePort().convention(8080);
-        getImageName().convention(project.getProviders().provider(project::getName));
+        getImageName().convention(provider(project, project::getName));
         getTags().convention(project.getProviders().provider(() -> List.of("latest", project.getVersion().toString())));
         getUseBuildx().convention(true);
         getPullForBuild().convention( false);
         getPush().convention(false);
         getLayered().convention(true);
+        getImageDescription().convention(provider(project, project::getDescription));
+        getImageTitle().convention(provider(project, project::getName));
+        getImageVersion().convention(provider(project, () -> project.getVersion().toString()));
+        getImageRevision().convention(provider(project,
+            // such as https://github.com/qoomon/gradle-git-versioning-plugin
+            lateAddedProperty(project, "git.commit")
+        ));
+    }
+
+    private static Callable<String> lateAddedProperty(Project project, String name) {
+        return () -> {
+            final Object value = project.findProperty(name);
+            return value != null ? value.toString() : null;
+        };
+    }
+
+    private static <T> Provider<T> provider(Project project, Callable<T> callable) {
+        return project.getProviders().provider(callable);
     }
 
 }
